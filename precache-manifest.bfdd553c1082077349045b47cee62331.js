740917ac8547c6081a0f31b7678cbc3dc00cad1c
O1dMate <67325830+O1dMate@users.noreply.github.com>
2020-07-06 17:27:09 +1000

    Template Site Initial

---
+++ precache-manifest.bfdd553c1082077349045b47cee62331.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41511095e33c2b5560cc5761e9f969cb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "68a02565a60146803912",
+    "url": "/static/js/2.2d3f2386.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.2d3f2386.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e30690af603cf656a03b",
+    "url": "/static/js/main.d4a1ef85.chunk.js"
+  },
+  {
+    "revision": "a5ee8943abb4eebdc79d",
+    "url": "/static/js/runtime-main.051d29b5.js"
+  }
+]);
