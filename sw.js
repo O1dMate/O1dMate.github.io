96e5217731ed65b2bd43515f93cec9e23956229b
O1dMate <67325830+O1dMate@users.noreply.github.com>
2021-04-08 21:59:42 +1000

    Dark mode default added

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/cache-list.js');
 
-var cacheName = 'chirpy-20210408.2002';
+var cacheName = 'chirpy-20210408.2158';
 
 function isExcluded(url) {
   const regex = /(^http(s)?|^\/)/; /* the regex for CORS url or relative url */
