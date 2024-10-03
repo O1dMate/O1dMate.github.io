54500f4c570b875524522499396656059c0ea6d3
O1dMate <o1dmate@protonmail.com>
2024-10-03 22:29:06 +1000

    Chirpy 7.1.1 Update

---
+++ assets/js/data/swconf.js
@@ -0,0 +1,1 @@
+const swconf = { cacheName: 'chirpy-1727958431',resources: [ '/assets/css/jekyll-theme-chirpy.css', '/', '/tools/', '/categories/', '/tags/', '/about/', '/archives/', ], interceptor: {paths: [ ],urlPrefixes: [ ] }, purge: false };
