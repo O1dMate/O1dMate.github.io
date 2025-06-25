c84f0479ecb91c74fcd37b60bb6ea273acf1dc34
O1dMate <o1dmate@protonmail.com>
2025-06-25 13:34:37 +1000

    JSON Parsing & Word List Generator

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,1 +1,1 @@
-const swconf = { cacheName: 'chirpy-1727958431',resources: [ '/assets/css/jekyll-theme-chirpy.css', '/', '/tools/', '/categories/', '/tags/', '/about/', '/archives/', ], interceptor: {paths: [ ],urlPrefixes: [ ] }, purge: false };
+const swconf = { cacheName: 'chirpy-1750768053',resources: [ '/assets/css/jekyll-theme-chirpy.css', '/', '/tools/', '/categories/', '/tags/', '/about/', '/archives/', ], interceptor: {paths: [ ],urlPrefixes: [ ] }, purge: false };
